98c7048f5a39b4065287b2366d61f4ba896e77f2
FionaWennberg <s246024@dtu.dk>
2026-05-17 11:09:21 +0200

    jobss

--- jobscript.sh
+++ jobscript.sh
@@ -1,39 +1,40 @@
-# !/bin/sh 
+#!/bin/bash
 # ## General options 
 # ## -- specify queue -- 
-# BSUB -q hpc
+#BSUB -q hpc
 # ## -- set the job Name -- 
-# BSUB -J TRF_NH_HI
+#BSUB -J TRF_NH_HI
 # ## -- ask for number of cores (default: 1) -- 
-# BSUB -n 4 
+#BSUB -n 4 
 # ## -- specify that the cores must be on the same host -- 
-# BSUB -R "span[hosts=1]"
+#BSUB -R "span[hosts=1]"
 # ## -- specify that we need 4GB of memory per core/slot -- 
-# BSUB -R "rusage[mem=8GB]"
+#BSUB -R "rusage[mem=8GB]"
 # ## -- specify that we want the job to get killed if it exceeds 5 GB per core/slot -- 
-# BSUB -M 8GB
+#BSUB -M 8GB
 # ## -- set walltime limit: hh:mm -- 
-# BSUB -W 24:00 
+#BSUB -W 24:00 
 # ## -- set the email address -- 
 # # please uncomment the following line and put in your e-mail address,
 # # if you want to receive e-mail notifications on a non-default address
-# BSUB -u s246024@dtu.dk
+#BSUB -u s246024@dtu.dk
 # ## -- send notification at start -- 
-# BSUB -B 
+#BSUB -B 
 # ## -- send notification at completion -- 
-# BSUB -N 
+#BSUB -N 
 # ## -- Specify the output and error file. %J is the job-id -- 
 # ## -- -o and -e mean append, -oo and -eo mean overwrite -- 
-# BSUB -o Output_%J.out 
-# BSUB -e Output_%J.err 
+#BSUB -o Output_%J.out 
+#BSUB -e Output_%J.err 
 
 cd /work3/s223643/AAD_Fagprojekt_v2
-module load python/3.11.9
+# module load python/3.11.9
 source ../venv/bin/activate
 
 python scripts/run_all_subjects.py \
   --bidsdir /work3/jhjort/ds-eeg-snhl \
   --processed-dir data/processed \
   --results-dir results_backward \
+  --subjects 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 \
   --step both \
   --max-workers 4
